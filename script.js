ecfa258afdf6942e1f4f68ce1beff5ed1ae3fb84
Jackson McHugh <jackson.mchugh@gmail.com>
2022-07-01 15:51:42 -0400

    half way done with hw

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ const API_KEY = '03676832f955ae00b509c34edbaad5f5'
 const DOMAIN = 'https://api.themoviedb.org/3'
 const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
 const button = document.querySelector('button')
-const picture = document.getElementsByClassName('movie-list')
+const picture = document.querySelector('.movie-list')
 const searchInput = document.querySelector('input')
 
 button.addEventListener('click', async () => {
   let search = searchInput.value
   const response = await axios.get(
-    'https://api.themoviedb.org/3/search/movie?query=[]&api_key=03676832f955ae00b509c34edbaad5f5'
+    `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${API_KEY}`
   )
-  let screen = response.data.results[0].title
-  picture.innerHtml = `<img`
-  console.log(screen)
+  let films = response.data.results
+
+  console.log(response)
+  renderList(films)
 })
+
+function renderList(arr) {
+  console.log(arr)
+  for (i = 0; i < arr.length; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = arr[i].original_title
+    picture.appendChild(li)
+    let image = document.createElement('img')
+    image.src = `${IMAGE_BASE_PATH}/${arr[i].poster_path}`
+    li.appendChild(image)
+  }
+}
